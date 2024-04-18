0dcb79f639d4c401c5b3e7e950e5623381d68e49
Aleksey <kniaz123312@gmail.com>
2024-04-18 15:58:03 +0500

    userController

--- Server/Controllers/userController.js
+++ Server/Controllers/userController.js
@@ -1,18 +1,22 @@
 const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt')
 class UserController {
-    async registration(req, res) {
+  async registration(req, res) {
+    const { email, password, role } = req.body
+    if (email) {
 
     }
-    async login(req, res) {
+  }
+  async login(req, res) {
 
+  }
+  async check(req, res, next) {
+    const id = req.query.id;
+    if (!id) {
+      return next(ApiError.badRequest('Не задано ID'));
     }
-    async check(req, res, next) {
-        const id=req.query.id;
-        if(!id){
-          return  next(ApiError.badRequest('Не задано ID'));
-        }
-        res.json(id);
-    }
+    res.json(id);
+  }
 }
 
 module.exports = new UserController()
