aa809c81a8f31aff2087fb3fee76618ac72bc9ad
Aleksey <kniaz123312@gmail.com>
2024-04-23 16:52:58 +0500

    ChekMiddleware

--- Server/Controllers/typeController.js
+++ Server/Controllers/typeController.js
@@ -1,16 +1,24 @@
-const {Type} = require('../Models/Models')
+const { Type } = require('../Models/Models')
 const ApiError = require('../error/ApiError');
 
 class TypeController {
-    async create(req, res) {
-        const {name} = req.body
-        const type = await Type.create({name})
-        return res.json(type)
+    async create(req, res, next) {
+        try {
+            const { name } = req.body
+            const type = await Type.create({ name })
+            return res.json(type)
+        } catch (e) {
+            next(ApiError.badRequest(e.message));
+        }
     }
 
-    async getAll(req, res) {
-        const types = await Type.findAll()
-        return res.json(types)
+    async getAll(req, res, next) {
+        try {
+            const types = await Type.findAll()
+            return res.json(types)
+        } catch (e) {
+            next(ApiError.badRequest(e.message));
+        }
     }
 
 }
--- Server/Controllers/userController.js
+++ Server/Controllers/userController.js
@@ -14,7 +14,9 @@ const generateJwt = (id, email, role) => {
   )
 }
 class UserController {
+ 
   async registration(req, res, next) {
+     try {
     const { email, password, role } = req.body
     if (!email || !password) {
       return next(ApiError.badRequest('Некорректный email или password'))
@@ -28,24 +30,28 @@ class UserController {
     const basket = await Basket.create({ userId: user.id })
     const token = generateJwt(user.id, user.email, user.role)
     return res.json({ token })
+  } catch (e) {
+    next(ApiError.badRequest(e.message));
+}
+
   }
   async login(req, res, next) {
     const { email, password } = req.body
-    const user = await User.findOne({where:{email}})
-    if(!user){
+    const user = await User.findOne({ where: { email } })
+    if (!user) {
       return next(ApiError.internal('пользователь с таким именем не найден'))
     }
     let comparePassword = bcrypt.compareSync(password, user.password)
-    if(!comparePassword){
+    if (!comparePassword) {
       return next(ApiError.internal('Неверный пароль'))
     }
     const token = generateJwt(user.id, user.email, user.role)
-    return res.json({token})
+    return res.json({ token })
   }
   async check(req, res, next) {
     const token = generateJwt(req.user.id, req.user.email, req.user.role)
-    return res.json({token})
-}
+    return res.json({ token })
+  }
 }
 
 module.exports = new UserController()
