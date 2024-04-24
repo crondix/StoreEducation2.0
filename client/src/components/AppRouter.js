58034f1cb7e16c61f037b6ecf8ad8f31ef18bb3c
Aleksey <kniaz123312@gmail.com>
2024-04-24 16:23:37 +0500

    It is working

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { Routes ,Route } from 'react-router-dom';
+import { authRoutes, publicRoutes } from '../routes';
+import { SHOP_ROUTE } from '../utils/consts';
+
+const AppRouter = () => {
+    const isAuth = true;
+    return (
+        <Routes>      
+            {isAuth && authRoutes.map(({ path, Component }) =>
+                <Route  key={path} path={path} element={<Component />}  />
+            )}
+            {publicRoutes.map(({ path, Component }) =>
+                <Route exact  key={path} path={path} element={<Component />}  />
+            )}
+            { <Route path="*"  element={<Shop/>}/> }
+        </Routes>
+    );
+};
+
+export default AppRouter;
---
+++ client/src/pages/DevicePage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class DevicePage extends Component {
+  render() {
+    return (
+      <div>DevicePage</div>
+    )
+  }
+}
