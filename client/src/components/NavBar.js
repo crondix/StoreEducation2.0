a8d86e183758cfb158c3a83d784e0f98418bb199
Aleksey <kniaz123312@gmail.com>
2024-04-27 16:36:59 +0500

    [FRONTEND] - Страница регистрации, авторизации

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -16,12 +16,12 @@ const NavBar = observer(() => {
                 <NavLink style={{ color: 'white' }} to={SHOP_ROUTE}>КупиДевайс</NavLink>
 
                 {user.isAuth ?
-                    <Nav className="ml-auto" style={{ color: 'white' }}>
+                    <Nav className="ms-auto" style={{ color: 'white' }}>
                         <Button variant={"outline-light"}>Админ панель</Button>
-                        <Button variant={"outline-light"}>Выйти</Button>
+                        <Button variant={"outline-light"} className="ms-2">Выйти</Button>
                     </Nav>
                     :
-                    <Nav className="ml-auto" style={{ color: 'white' }}>
+                    <Nav className="ms-auto" style={{ color: 'white' }}>
                         <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
                     </Nav>
                 }
